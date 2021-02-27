652767c74a75084b55b7c29354436422a7f6cd99
Karl Meakin <karlwfmeakin@gmail.com>
2021-02-27 04:19:24 +0000

    Monoid docs

--- src/monoid.rs
+++ src/monoid.rs
@@ -3,10 +3,26 @@ use std::{
     hash::{BuildHasher, Hash},
 };
 
+/// A Monoid is type with an associative binary operator, `combine`, and an
+/// identity element, `unit`.
+/// Implementations must obey the following *laws*:
+/// - **Left identity**:  forall x. `T::unit().combine(x) == T::unit()`
+/// - **Right identity**: forall x. `x.combine(T::unit()) == T::unit()`
+/// - **Associativity**:  forall x, y, z. `x.combine(y.combine(z)) ==
+///   (x.combine(y)).combine(z)`
 pub trait Monoid {
+    #[doc(alias = "mempty")]
+    /// The identity element
     fn unit() -> Self;
 
+    #[doc(alias = "<>")]
+    #[doc(alias = "mappend")]
+    /// The associative binary operator
     fn combine(self, other: Self) -> Self;
+
+    #[doc(alias = "mconcat")]
+    /// Combine all the elements of an iterator by repeated application of
+    /// combine: `concat([e₁, e₂, ..., eₙ]) == e₁.combine(e₂) ... .combine(eₙ)`
     fn concat<I>(iter: I) -> Self
     where
         I: IntoIterator<Item = Self>,
@@ -16,6 +32,7 @@ pub trait Monoid {
     }
 }
 
+/// Combine `String`s by appending their contents.
 impl Monoid for String {
     fn unit() -> Self { Self::default() }
     fn combine(mut self, other: Self) -> Self {
@@ -24,6 +41,8 @@ impl Monoid for String {
     }
 }
 
+/// Combine monoids wrapped in `Option` by treating each `Option` as a container
+/// containing either 0 or 1 elements.
 impl<T: Monoid> Monoid for Option<T> {
     fn unit() -> Self { Self::default() }
     fn combine(self, other: Self) -> Self {
@@ -35,6 +54,7 @@ impl<T: Monoid> Monoid for Option<T> {
     }
 }
 
+/// Combine `Vec`s by appending thier contents
 impl<T> Monoid for Vec<T> {
     fn unit() -> Self { Self::default() }
     fn combine(mut self, other: Self) -> Self {
@@ -43,6 +63,7 @@ impl<T> Monoid for Vec<T> {
     }
 }
 
+/// Combine `VecDeque`s by appending thier contents
 impl<T> Monoid for VecDeque<T> {
     fn unit() -> Self { Self::default() }
     fn combine(mut self, other: Self) -> Self {
@@ -51,6 +72,7 @@ impl<T> Monoid for VecDeque<T> {
     }
 }
 
+/// Combine `LinkedList`s by appending thier contents
 impl<T> Monoid for LinkedList<T> {
     fn unit() -> Self { Self::default() }
     fn combine(mut self, other: Self) -> Self {
@@ -59,6 +81,8 @@ impl<T> Monoid for LinkedList<T> {
     }
 }
 
+/// Combine `HashSet`s by taking the union of the two sets (second set takes
+/// priority)
 impl<T: Eq + Hash, S: BuildHasher + Default> Monoid for HashSet<T, S> {
     fn unit() -> Self { Self::default() }
     fn combine(mut self, other: Self) -> Self {
@@ -67,6 +91,8 @@ impl<T: Eq + Hash, S: BuildHasher + Default> Monoid for HashSet<T, S> {
     }
 }
 
+/// Combine `BTreeSet`s by taking the union of the two sets (second set takes
+/// priority)
 impl<T: Ord> Monoid for BTreeSet<T> {
     fn unit() -> Self { Self::default() }
     fn combine(mut self, other: Self) -> Self {
@@ -75,6 +101,8 @@ impl<T: Ord> Monoid for BTreeSet<T> {
     }
 }
 
+/// Combine `HashMap`s by taking the union of the two maps (second map takes
+/// priority)
 impl<K: Eq + Hash, V, S: BuildHasher + Default> Monoid for HashMap<K, V, S> {
     fn unit() -> Self { Self::default() }
     fn combine(mut self, other: Self) -> Self {
@@ -83,6 +111,8 @@ impl<K: Eq + Hash, V, S: BuildHasher + Default> Monoid for HashMap<K, V, S> {
     }
 }
 
+/// Combine `BTreeMap`s by taking the union of the two maps (second map takes
+/// priority)
 impl<K: Ord, V> Monoid for BTreeMap<K, V> {
     fn unit() -> Self { Self::default() }
     fn combine(mut self, other: Self) -> Self {
